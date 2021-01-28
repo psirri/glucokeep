cc705f3fb44c47a9a9c809c9d9366c01fdc0bc87
Paul Sirri <paulsirri@gmail.com>
2021-01-27 22:33:11 -0500

    Add GPL boilerplate to python files

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,3 +1,21 @@
+"""
+main.py -- Launching point for automated distribution package (FBS).
+Copyright (C) 2021  Paul Sirri <paulsirri@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
 from PyQt5.QtWidgets import QMainWindow
 from windows import StartWindow
